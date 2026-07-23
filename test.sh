1a92f003219637749e03e37f2335923b97160855
Ram Haridas <43177945+RamHaridas@users.noreply.github.com>
2026-07-23 22:02:23 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 USERNAME="your email"
 GITHUB_PAT="pattoken"
-
-echo '@bsd:registry=https://artifactory.fis.dev/artifactory/api/npm/bsd-npm-dev/' >> .npmrc
-echo '//artifactory.fis.dev/artifactory/api/npm/bsd-npm-dev/:_auth=${AUTH_BASE64}' >> .npmrc
+AUTH_BASE64=$(echo -n "${USERNAME}:${GITHUB_PAT}" | base64)
+echo "@bsd:registry=https://artifactory.fis.dev/artifactory/api/npm/bsd-npm-dev/" >> .npmrc
+echo "//artifactory.fis.dev/artifactory/api/npm/bsd-npm-dev/:_auth=${AUTH_BASE64}" >> .npmrc
